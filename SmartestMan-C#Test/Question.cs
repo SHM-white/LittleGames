87a27ac0bc99143c380c686201cfc20375ed5966
White SHM <1131287310a@gmail.com>
2023-05-07 23:20:49 +0800

    www.codeconvert.ai转换

--- SmartestMan-C#Test/Question.cs
+++ SmartestMan-C#Test/Question.cs
@@ -1,62 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SmartestMan_C_Test
+class Question
 {
-    public class Question
+    public int m_score;
+    private int m_leftTime = 0;
+    private int m_firstNumber;
+    private int m_secondNumber;
+    private int m_correctAnswer;
+    private int m_notCorrectAnswer;
+    private bool m_isAnswerCorrect;
+    private enum operators
     {
-        public string question;
-        public string[] answers;
-        public int correctAnswer;
+        add,
+        minus,
+        multiply,
+        divide
+    };
+    private operators op;
+    private int m_result;
+    private int m_difficulty = 5;
 
-        public Question(string question, string[] answers, int correctAnswer)
+    public Question(int difficulty)
+    {
+        m_difficulty = difficulty;
+    }
+
+    public string getQuestion()
+    {
+        return $"{m_firstNumber} {op} {m_secondNumber} = ?";
+    }
+
+    public int TickRun(int tick)
+    {
+        m_leftTime -= tick;
+        if (m_leftTime <= 0)
         {
-            this.question = question;
-            this.answers = answers;
-            this.correctAnswer = correctAnswer;
+            m_leftTime = 0;
+            return 0;
         }
+        return m_leftTime;
+    }
 
-        public static List<Question> LoadQuestions(string fileName)
+    public bool isCorrect(bool answer)
+    {
+        if (answer == m_isAnswerCorrect)
         {
-            List<Question> questions = new List<Question>();
-            using (StreamReader reader = new StreamReader(fileName, Encoding.UTF8))
-            {
-                while (!reader.EndOfStream)
+            m_score += m_difficulty;
+            return true;
+        }
+        else
+        {
+            m_score -= m_difficulty;
+            return false;
+        }
+    }
+
+    public bool newQuestion()
+    {
+        bool haveNegative = true;
+        if (m_difficulty < 0)
+        {
+            haveNegative = false;
+        }
+        if (!spawnNumber(m_difficulty, haveNegative))
+        {
+            return false;
+        }
+        setLeftTime();
+        return true;
+    }
+
+    public bool setDifficulty(int difficulty)
+    {
+        if (difficulty < 1 || difficulty > 10)
+        {
+            return false;
+        }
+        m_difficulty = difficulty;
+        return true;
+    }
+
+    private bool spawnNumber(int range, bool haveNegative)
+    {
+        Random rnd = new Random();
+        int first = rnd.Next(0, range);
+        int second = rnd.Next(0, range);
+        if (haveNegative)
+        {
+            first = rnd.Next(-range, range);
+            second = rnd.Next(-range, range);
+        }
+        op = (operators)rnd.Next(0, 4);
+        switch (op)
+        {
+            case operators.add:
+                m_correctAnswer = first + second;
+                break;
+            case operators.minus:
+                m_correctAnswer = first - second;
+                break;
+            case operators.multiply:
+                m_correctAnswer = first * second;
+                break;
+            case operators.divide:
+                if (second == 0)
                 {
-                    string line = reader.ReadLine();
-                    if (!string.IsNullOrEmpty(line))
-                    {
-                        string[] parts = line.Split('|');
-                        if (parts.Length == 6)
-                        {
-                            string question = parts[0];
-                            string[] answers = new string[4];
-                            for (int i = 0; i < 4; i++)
-                            {
-                                answers[i] = parts[i + 1];
-                            }
-                            int correctAnswer = int.Parse(parts[5]);
-                            questions.Add(new Question(question, answers, correctAnswer));
-                        }
-                    }
+                    return false;
                 }
-            }
-            return questions;
+                m_correctAnswer = first / second;
+                break;
         }
-
-        public override string ToString()
+        m_firstNumber = first;
+        m_secondNumber = second;
+        m_isAnswerCorrect = true;
+        if (rnd.Next(0, 2) == 0)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine(this.question);
-            for (int i = 0; i < 4; i++)
-            {
-                sb.AppendLine((i + 1) + ". " + this.answers[i]);
-            }
-            return sb.ToString();
+            m_notCorrectAnswer = m_correctAnswer + rnd.Next(1, 10);
+            m_isAnswerCorrect = false;
         }
+        else
+        {
+            m_notCorrectAnswer = m_correctAnswer - rnd.Next(1, 10);
+        }
+        return true;
+    }
+
+    private bool setLeftTime()
+    {
+        m_leftTime = m_difficulty * 10;
+        return true;
     }
 }
